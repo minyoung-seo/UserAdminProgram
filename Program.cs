b114b602d05ae72257577e8cf08a96c89c76f097
minyoung-seo <tjalsdud2006@naver.com>
2019-12-08 21:23:13 +0900

    useradmin

---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UserAdminProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserManager manager = new UserManager();
+            
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("** 회원관리 프로그램 **");
+                Console.WriteLine("[1] 등록하기");
+                Console.WriteLine("[2] 불러오기(List)");
+                Console.WriteLine("[3] 상세보기");
+                Console.WriteLine("[4] 저장하기");
+                Console.WriteLine("[5] 불러오기");
+                Console.WriteLine("[6] 종료");
+
+                SelectMenuNum selectmenunumber = (SelectMenuNum)Int32.Parse(Console.ReadLine());
+
+                switch (selectmenunumber)
+                {
+                    case SelectMenuNum.Regesiter:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("[1] 신규회원 등록하기---");
+                            manager.CreateUser(Input());
+                            break;
+                        }
+
+                    case SelectMenuNum.List:
+                        {
+                            bool editpage = true;
+                            while (editpage)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("[2] 불러오기(List)");
+                                DisplayUsers(manager.Users);
+                                Console.WriteLine("------------END------------");
+                                Console.WriteLine("[1] Edit User");
+                                Console.WriteLine("[2] Delete User");
+                                Console.WriteLine("[3] Find User by Name");
+                                Console.WriteLine("[4] Back to the Mainpage");
+                                Console.WriteLine("[5] Exit");
+
+                                EditMenuNum editmenumumber = (EditMenuNum)Int32.Parse(Console.ReadLine());
+
+                                switch(editmenumumber)
+                                {
+                                    case EditMenuNum.Edit:
+                                        {
+                                            Console.Write("[Edit] which number? : ");
+                                            int editnumber = Int32.Parse(Console.ReadLine());
+                                            manager.EditUser(editnumber, Input());
+                                            break;
+                                        }
+                                    case EditMenuNum.Delete:
+                                        {
+                                            Console.Write("[Delete] which number? :");
+                                            int deletenumber = Int32.Parse(Console.ReadLine());
+                                            manager.DeleteUser(deletenumber);
+                                            break;
+                                        }
+                                    case EditMenuNum.Find:
+                                        {
+                                            Console.Write("Name : ");
+                                            List<User> findresult = manager.FindByName(Console.ReadLine());
+                                            DisplayUsers(findresult);
+                                            string pause = Console.ReadLine();
+                                            break;
+                                        }
+                                    default:
+                                    case EditMenuNum.BackToMain:
+                                        {
+                                            editpage = false;
+                                            break;
+                                        }
+                                    case EditMenuNum.Exit:
+                                        {
+                                            return;
+                                        }
+                                   
+                                }
+                            }
+                            break;
+                        }
+                    case SelectMenuNum.Definite:
+                        {
+                            bool step = true;
+                            while(step)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("[3] 상세보기");
+                                DisplayDetailUsers(manager.Users);
+                                string pause = Console.ReadLine();
+                                step = false;
+                                
+                            }
+                            break;
+                        }
+
+                    case SelectMenuNum.Save:
+                        {
+                            Console.WriteLine("File name : ");
+                            string filename = Console.ReadLine();
+                            manager.SaveFile(filename);
+                            Console.WriteLine("---완료!---");
+                            System.Threading.Thread.Sleep(300);
+                            break;
+                        }
+
+                    case SelectMenuNum.Load:
+                        {
+                            Console.WriteLine("File name : ");
+                            string filename = Console.ReadLine();
+                            manager.LoadFile(filename);
+                            Console.WriteLine("---완료!---");
+                            System.Threading.Thread.Sleep(300);
+                            break;
+                        }
+
+                    default:
+                    case SelectMenuNum.Exit:
+                        return;
+                }
+            }
+        }
+        
+        
+        public static User Input()
+        {
+            Console.Write("Name : ");
+            string name = Console.ReadLine();
+            Console.Write("Phone : ");
+            string phone = Console.ReadLine();
+            Console.Write("Age : ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Sex : ");
+            string sex = Console.ReadLine();
+
+            return new User(name, phone, age, sex);
+        }
+
+        public static void DisplayUsers(List<User> Users)
+        {
+            int count = 1;
+            foreach (User i in Users)
+            {
+                Console.Write("<{0}>", count);
+                Console.WriteLine(i.GetUserInfo());
+                count++;
+            }
+        }
+
+        public static void DisplayDetailUsers(List<User> Users)
+        {
+            int count = 1;
+            foreach(User i in Users)
+            {
+                Console.Write("<{0}>", count);
+                Console.WriteLine(i.GetDetailUserInfo());
+                count++;
+            }
+        }
+
+        public enum SelectMenuNum
+        {
+            Regesiter = 1,
+            List,
+            Definite,
+            Save,
+            Load,
+            Exit
+        }
+
+        public enum EditMenuNum
+        {
+            Edit=1,
+            Delete,
+            Find,
+            BackToMain,
+            Exit
+        }
+
+    }   
+ }
+
---
+++ UserManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UserAdminProgram
+{
+    class UserManager
+    {
+        public List<User> Users = new List<User>();
+        
+        public void CreateUser(User newUser)
+        {
+            Users.Add(newUser);
+        }
+
+        public void EditUser(int editnumber, User newUser)
+        {
+            Users[editnumber] = newUser;
+        }
+
+        public void DeleteUser(int deletenumber)
+        {
+            Users.RemoveAt(deletenumber);
+        }
+
+        public List<User> FindByName(string name)
+        {
+            int count = 0;
+            List<User> findresult = new List<User>();
+            foreach(User e in Users)
+            {
+                if(e.Name == name)
+                {
+                    findresult.Add(e);
+                }
+                count++;
+            }
+            return findresult;
+        }
+
+        public void SaveFile(string filename)
+        {
+            StreamWriter savefile = new StreamWriter(@"C:\Users\tjals\desktop\practice\" + filename + ".txt");
+            foreach(User e in Users)
+            {
+                savefile.WriteLine(e.Name);
+                savefile.WriteLine(e.Phone);
+                savefile.WriteLine(e.Age);
+                savefile.WriteLine(e.Sex);
+            }
+            savefile.Close();
+        }
+
+        public void LoadFile(string filename)
+        {
+            try
+            {
+                using (StreamReader loadfile = new StreamReader(@"C:\Users\tjals\desktop\practice\" + filename + ".txt"))
+                {
+                    string line;
+                    while ((line = loadfile.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
